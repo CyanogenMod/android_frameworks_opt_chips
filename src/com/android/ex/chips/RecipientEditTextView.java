a46013cea35cff70b89db7825285b579c4f8ab0e
Steve Kondik <steve@cyngn.com>
2016-08-29 10:46:43 -0700

    chips: Adjust for N changes
    Change-Id: I72c81e86ad48319c3f3c137985b6d683fa17821c

--- src/com/android/ex/chips/RecipientEditTextView.java
+++ src/com/android/ex/chips/RecipientEditTextView.java
@@ -1410,7 +1410,7 @@ public class RecipientEditTextView extends MultiAutoCompleteTextView implements
     }
 
     private String tokenizeAddress(String destination) {
-        if (getAdapter().isPhoneQuery() && isPhoneNumber(destination)) {
+        if (getAdapter().isPhoneQuery() && PhoneUtil.isPhoneNumber(destination)) {
             return PhoneNumberUtils.formatNumberToE164(destination,
                 Locale.getDefault().getCountry());
         } else {
@@ -3334,6 +3334,6 @@ public class RecipientEditTextView extends MultiAutoCompleteTextView implements
      * @return true if still tokenizing contacts, false otherwise
      */
     protected boolean isChipping() {
-        return !mNoChips;
+        return !mNoChipMode;
     }
 }
